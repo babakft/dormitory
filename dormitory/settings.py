27203ace1feed39e3b58cbbe45dae9de521a6c78
babak <babakfathi082@gmail.com>
2025-07-10 13:06:35 +0330

    -add AUTH_MODULE to setting -correct the database connection detail - add .idea to .gitignore

--- dormitory/settings.py
+++ dormitory/settings.py
@@ -123,5 +123,5 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
-
+AUTH_USER_MODEL = 'student.User'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
