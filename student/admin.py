0ebde5004cd278a31e21d49f772b9a1a35b5292a
babak <babakfathi082@gmail.com>
2025-07-12 15:45:55 +0330

    _ add building and room _ create data for these fields

--- student/admin.py
+++ student/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from student.models import Building, Room
 
-# Register your models here.
+
+@admin.register(Building)
+class BuildingAdmin(admin.ModelAdmin):
+    list_display = ['name', 'total_floors', 'created_at']
+    search_fields = ['name']
+    list_filter = ['total_floors', 'created_at']
+    ordering = ['name']
+
+
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ['building', 'number', 'floor', 'capacity', 'student_count']
+    list_filter = ['building', 'floor', 'capacity']
+    search_fields = ['number', 'building__name']
+    ordering = ['building', 'floor', 'number']
+
+    def student_count(self, obj):
+        return obj.students.count()
+
+    student_count.short_description = 'Students'
